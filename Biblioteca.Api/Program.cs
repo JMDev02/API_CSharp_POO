45c54f77f6966fc88440b91a459ecb6e980dfe52
João Marcelo <joaomarcelonunes02@gmail.com>
2026-08-21 21:14:24 -0300

    feat: rota/itens e redirect do endpoint/

--- Biblioteca.Api/Program.cs
+++ Biblioteca.Api/Program.cs
@@ -1,8 +1,15 @@
+using Biblioteca.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+var acervo = new Acervo();
+var cadastro = new Cadastro();
+Seed.Popular(acervo, cadastro);
 
+app.MapGet("/", () => Results.Redirect("/itens"));
+
+app.MapGet("/itens",()=>acervo.Itens);
 
 app.Run();
